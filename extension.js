0b1265ab45aba7b9739823d377fac10bc152c671
Jordi Mas <jmas@softcatala.org>
2019-08-27 17:56:49 +0200

    Show microphone

--- extension.js
+++ extension.js
@@ -55,10 +55,56 @@ const MicroCameraPrivacy = class MicroCameraPrivacy
         return true;
     }
 
+
+  _show_microphone() {
+        global.log('_show_microphone called');
+        this.microphone_button = new St.Bin({ style_class: 'panel-button',
+                              reactive: true,
+                              can_focus: true,
+                              x_fill: true,
+                              y_fill: false,
+                              track_hover: true });
+
+        let icon = new St.Icon({ icon_name: 'audio-input-microphone-symbolic.symbolic',
+                                 style_class: 'system-status-icon' });
+
+        this.microphone_button.set_child(icon);
+        Main.panel._rightBox.insert_child_at_index(this.microphone_button, 0);
+    }
+
+    _hide_microphone() {
+        global.log('_hide_microphone called');
+        Main.panel._rightBox.remove_child(this.microphone_button);
+    }
+
+    _check_microphone() {
+        global.log('_check_microphone started');
+
+        let array = GLib.spawn_command_line_sync("pacmd list-source-outputs")[1];
+        let stuff = ByteArray.toString(array);
+        global.log("pacmd list-source-outputs: [" + stuff +"]");
+
+        if (stuff.includes('state: RUNNING')) {
+            if (this.microphone_enabled == false)
+                 this._show_microphone();
+    
+            this.microphone_enabled = true;
+        } else {
+           if (this.microphone_enabled == true)
+                 this._hide_microphone();
+    
+            this.microphone_enabled = false;
+        }
+        global.log('_check_microphone called finished');
+        return true;
+    }
+
+
     enable() {
         global.log("Enable start");
-        let timer = 5000;
-        this._webcam_timer = Mainloop.timeout_add(timer, Lang.bind(this, this._check_webcam));
+        this._webcam_timer = Mainloop.timeout_add(3000, Lang.bind(this, this._check_webcam));
+        this._microphone_timer = Mainloop.timeout_add(3000, Lang.bind(this, this._check_microphone));
+
         global.log("Enable end");
     }
 
@@ -66,6 +112,10 @@ const MicroCameraPrivacy = class MicroCameraPrivacy
         global.log("Disable start");
         Mainloop.source_remove(this._webcam_timer);
         this._hide_webcam();
+        
+        Mainloop.source_remove(this._microphone_timer);
+        this._hide_microphone();
+
         global.log("Disable end");
     }
 };
